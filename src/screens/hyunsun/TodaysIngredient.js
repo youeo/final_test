10bb4967637aef450ecf8145353b93207f9e5426
hyun5un <144610317+hyun5un@users.noreply.github.com>
2025-08-19 11:42:04 +0900

    Update TodaysIngredient.js

--- src/screens/hyunsun/TodaysIngredient.js
+++ src/screens/hyunsun/TodaysIngredient.js
@@ -1,12 +1,20 @@
 import { View, Text, Image, ScrollView, TouchableOpacity, StatusBar } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { ChevronLeftIcon } from 'react-native-heroicons/outline';
 import Animated, { FadeInDown } from 'react-native-reanimated';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
-export default function IngredientInfoScreen() {
+export default function TodaysIngredientScreen() {
   const navigation = useNavigation();
+  const [ingredient, setIngredient] = useState(null);
+
+  useEffect(() => {
+    fetch('http://43.200.200.161:8080/Ingredienttoday')
+      .then(res => res.json())
+      .then(data => setIngredient(data))
+      .catch(err => console.error(err));
+  }, []);
 
   return (
     <View className="flex-1 bg-white">
@@ -29,23 +37,27 @@ export default function IngredientInfoScreen() {
             오늘의 식재료 상식
           </Text>
 
-          <View className="items-center">
-            <Image
-              source={require('../../../assets/images/carrot.jpg')}
-              style={{ width: wp(80), height: hp(25), borderRadius: 20 }}
-              className="bg-black/5"
-              resizeMode="cover"
-            />
-          </View>
+          {/* 이미지 */}
+          {ingredient && (
+            <View className="items-center">
+              <Image
+                source={{ uri: ingredient.image }}
+                style={{ width: wp(80), height: hp(25), borderRadius: 20 }}
+                className="bg-black/5"
+                resizeMode="cover"
+              />
+            </View>
+          )}
 
-          <View className="space-y-2 pt-4">
-            <Text className="text-xl font-bold text-center">당근</Text>
-            <Text className="text-base text-neutral-700 text-justify leading-6">
-              당근에는 베타카로틴, 비타민 A, 펙틴, 리그닌 등이 풍부하게 함유되어 있어 다양한 효능이 있습니다. 시력 보호, 암 예방, 혈압/혈당 조절,
-              변비 개선, 피부 질환 치료, 피로 회복 등 건강에 매우 유익한 식재료입니다. 당근을 고를 때는 색이 진하고, 껍질이 매끄러우며, 단단하고
-              휘지 않은 것을 선택하는 것이 좋습니다.
-            </Text>
-          </View>
+          {/* 이름 + 설명 */}
+          {ingredient && (
+            <View className="space-y-2 pt-4">
+              <Text className="text-xl font-bold text-center">{ingredient.name}</Text>
+              <Text className="text-base text-neutral-700 text-justify leading-6">
+                {ingredient.description}
+              </Text>
+            </View>
+          )}
         </Animated.View>
       </ScrollView>
     </View>
